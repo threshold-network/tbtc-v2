91fa4e13e4ea70e13582aefe737e4eed6d62c59b
maclane <maclane@nucypher.com>
2026-06-03 14:41:02 -0500

    Support P2PKH deposit funding inputs

--- typescript/src/services/deposits/funding.ts
+++ typescript/src/services/deposits/funding.ts
@@ -45,7 +45,7 @@ export class DepositFunding {
    * @param bitcoinNetwork The target Bitcoin network.
    * @param amount Deposit amount in satoshis.
    * @param inputUtxos UTXOs to be used for funding the deposit transaction.
-   *                   So far only P2WPKH UTXO inputs are supported.
+   *                   So far only P2PKH and P2WPKH UTXO inputs are supported.
    * @param fee Transaction fee to be subtracted from the sum of the UTXOs' values.
    * @param depositorPrivateKey Bitcoin private key of the depositor. Must
    *        be able to unlock input UTXOs.
@@ -90,8 +90,6 @@ export class DepositFunding {
       const previousOutputValue = previousOutput.value
       const previousOutputScript = previousOutput.script
 
-      // TODO: Add support for other utxo types along with unit tests for the
-      //       given type.
       if (BitcoinScriptUtils.isP2WPKHScript(Hex.from(previousOutputScript))) {
         psbt.addInput({
           hash: utxo.transactionHash.reverse().toBuffer(),
@@ -100,6 +98,19 @@ export class DepositFunding {
             script: previousOutputScript,
             value: previousOutputValue,
           },
+        })
+
+        totalInputValue = totalInputValue.add(utxo.value)
+        if (totalInputValue.gte(totalExpenses)) {
+          break
+        }
+      } else if (
+        BitcoinScriptUtils.isP2PKHScript(Hex.from(previousOutputScript))
+      ) {
+        psbt.addInput({
+          hash: utxo.transactionHash.reverse().toBuffer(),
+          index: utxo.outputIndex,
+          nonWitnessUtxo: Buffer.from(utxo.transactionHex, "hex"),
         })
 
         totalInputValue = totalInputValue.add(utxo.value)
@@ -162,7 +173,7 @@ export class DepositFunding {
    *      to ensure the given deposit is funded exactly once.
    * @param amount Deposit amount in satoshis.
    * @param inputUtxos UTXOs to be used for funding the deposit transaction. So
-   *        far only P2WPKH UTXO inputs are supported.
+   *        far only P2PKH and P2WPKH UTXO inputs are supported.
    * @param fee The value that should be subtracted from the sum of the UTXOs
    *        values and used as the transaction fee.
    * @param depositorPrivateKey Bitcoin private key of the depositor.
