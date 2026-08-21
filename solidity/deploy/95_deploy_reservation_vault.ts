48bc9f0369914734f7a9394334636998a37ce1f0
Piotr Rosłaniec <p.roslaniec@gmail.com>
2026-08-21 16:26:53 +0000

    fix(deploy): declare Bridge as dependency of ReservationVault deploy script

--- solidity/deploy/95_deploy_reservation_vault.ts
+++ solidity/deploy/95_deploy_reservation_vault.ts
@@ -38,4 +38,4 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 export default func
 
 func.tags = ["ReservationVault"]
-func.dependencies = ["Bank", "TBTCVault"]
+func.dependencies = ["Bank", "TBTCVault", "Bridge"]
